a8456fe8e2c5ae39cac62d1c7d6496d2fe2f1c47
Swati Singh <swatisingh@Swatis-MacBook-Pro.local>
2017-11-30 15:56:23 +0100

    Removed dummy expense data

--- expensifyApp/src/app.js
+++ expensifyApp/src/app.js
@@ -12,17 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-})
-
-
-store.dispatch(addExpense({description : "Water Bill", amount : 1800}));
-store.dispatch(addExpense({description : "Gas Bill", createdAt: 1000}));
-store.dispatch(addExpense({description : "Rent", amount : 109500}));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
